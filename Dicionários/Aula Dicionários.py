de1a8a31ec454af5e8beb7e7a87113ed1e21c9ac
André Rocha <andrenakarocha@hotmail.com>
2024-09-09 08:49:28 -0300

    return_polygon - Dicionários

--- Dicionários/Aula Dicionários.py
+++ Dicionários/Aula Dicionários.py
@@ -1,15 +1,36 @@
-def dic_odd_and_evens(numbers):
-    dic = {
-        'odds': [],
-        'evens': []
+def dic_even_and_odds(numbers):
+    dict = {
+        'evens': [],
+        'odds': []
     }
 
     for num in numbers:
         if num % 2 == 0:
-            dic['evens'].append(num)
+            dict['evens'].append(num)
         else:
-            dic['odds'].append(num)
-    print(dic)
+            dict['odds'].append(num)
+    print(dict)
     return
 
-dic_odd_and_evens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+def return_polygon(number_of_sides):
+    polygons = {
+        3: "Triangle",
+        4: "Square",
+        5: "Pentagon",
+        6: "Hexagon",
+        7: "Heptagon",
+        8: "Octagon"
+    }
+    try:
+        print(polygons[number_of_sides])
+        return polygons[number_of_sides]
+    except KeyError:
+        print("ta querendo demais")
+    return
+
+
+# dic_even_and_odds([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+# Cria dicionário com pares e ímpares de números na lista
+
+# return_polygon(5)
+# Retorna o polígono conforme o número de lados informados
