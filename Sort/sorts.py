28ec61ce9820835e5fb2565023d5c4a6e75d93a3
André Rocha <andrenakarocha@hotmail.com>
2024-10-07 09:38:50 -0300

    bubble-sort

--- Sort/sorts.py
+++ Sort/sorts.py
@@ -1,17 +1,41 @@
-def achar_menor (lista):
-    indice_menor = 0
-    menor = lista[indice_menor]
-    for i in range(len(lista)):
-        if lista[i] < menor:
-            menor = lista[i]
-            indice_menor = i
-    return indice_menor
+def find_min (list):
+    index_min = 0
+    min = list[index_min]
+    for i in range(len(list)):
+        if list[i] < min:
+            min = list[i]
+            index_min = i
+    return index_min
 
-def selection_sort(lista):
-    lista_ordenada = []
-    while lista:
-        menor = achar_menor(lista)
-        elemento = lista.pop(menor)
-        lista_ordenada.append(elemento)
-    return lista_ordenada
+def very_bad_selection_sort(list):
+    sorted_list = []
+    while list:
+        min = find_min(list)
+        element = list.pop(min)
+        sorted_list.append(element)
+    return sorted_list
+
+
+def bad_selection_sort(list):
+    for i in range(len(list)):
+        index_min = find_min(list[i:]) + i
+        aux = list[i]
+        list[i] = list[index_min]
+        list[index_min] = aux
+    return
+
+
+def bubble_sort(list):
+    for j in range(len(list)):
+        num_of_changes = 0
+        for i in range(len(list) - 1 - j):
+            if list[i] > list[i + 1]:
+                aux = list[i]
+                list[i] = list[i + 1]
+                list[i + 1] = aux
+                num_of_changes += 1
+        if num_of_changes == 0:
+            break
+    return
+
 
